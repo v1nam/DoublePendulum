a0bb5703b13c7f3ee8eaa4fe4999b2ee5f6c534b
v1nam <vinamraj01@gmail.com>
2021-08-12 14:52:14 +0530

    change project structure

---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+#include <cmath>
+
+#include "raylib.h"
+#include "pendulum.hpp"
+#include "display.hpp"
+
+int main()
+{
+    const int screenWidth = 600;
+    const int screenHeight = 700;
+
+    bool paused = true;
+    //Vector2 oldTrailPos = Vector2{-1., 0.};
+
+    SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(screenWidth, screenHeight, "Double Pendulum");
+
+    DoublePendulum pend1 = DoublePendulum(Pendulum{9.0, 150, PI / 2.0, Vector2{screenWidth / 2, 350.0}, Vector2{screenWidth / 2, 500.0}, false, 10.0}, Pendulum{9.0, 150, PI / 2.0, Vector2{screenWidth / 2, 500.0}, Vector2{screenWidth / 2, 650.0}, false, 10.0});
+    DoublePendulum pend2 = DoublePendulum(Pendulum{9.0, 150, PI / 2.0 + 0.01, Vector2{screenWidth / 2, 350.0}, Vector2{screenWidth / 2, 500.0}, false, 10.0}, Pendulum{9.0, 150, PI / 2.0 + 0.01, Vector2{screenWidth / 2, 500.0}, Vector2{screenWidth / 2, 650.0}, false, 10.0});
+
+    /*
+    RenderTexture2D traceBG = LoadRenderTexture(screenWidth, screenHeight);
+    BeginTextureMode(traceBG);
+    ClearBackground(RAYWHITE);
+    EndTextureMode();
+    */
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        if (IsKeyPressed(KEY_SPACE))
+            paused = !paused;
+        /*
+        if (IsKeyPressed(KEY_C))
+        {
+            oldTrailPos.x = -1.;
+            BeginTextureMode(traceBG);
+            ClearBackground(RAYWHITE);
+            EndTextureMode();
+        }
+        */
+
+        if (paused)
+        {
+            handleBobDrag(pend1);
+            handleBobDrag(pend2);
+        }
+        else
+        {
+            pend1.moveObjects();
+            pend2.moveObjects();
+            /*
+            BeginTextureMode(traceBG);
+            if (oldTrailPos.x != -1.)
+            {
+                DrawLine(pend1.p2.position.x, screenHeight - pend1.p2.position.y, oldTrailPos.x, screenHeight - oldTrailPos.y, RED);
+            }
+            EndTextureMode();
+            oldTrailPos = pend1.p2.position;
+            */
+        }
+
+        pend1.updatePos();
+        pend2.updatePos();
+
+        //DrawTexture(traceBG.texture, 0, 0, WHITE);
+        drawDoublePend(pend1, BLACK);
+        drawDoublePend(pend2, BLUE);
+        DrawFPS(10, 10);
+
+        EndDrawing();
+    }
+    //UnloadRenderTexture(traceBG);
+    CloseWindow();
+    return 0;
+}
