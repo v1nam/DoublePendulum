ccb80c4eab34000e40a67e70583906a554583e07
v1nam <vinamraj01@gmail.com>
2021-10-04 19:29:03 +0530

    add constructor

--- include/pendulum.hpp
+++ include/pendulum.hpp
@@ -9,9 +9,16 @@ struct Pendulum
     double angle;
     Vector2 start;
     Vector2 position;
+    Vector2 oldPos;
     bool moveBob;
     float mass;
     double velocity{0.0};
+
+    Pendulum(float r, int sl, double a, Vector2 st, Vector2 p, float m) : 
+        radius(r), stringLen(sl), angle(a),
+        start(st), position(p), oldPos(p),
+        mass(m), moveBob(false) {}
+    Pendulum() {}
 };
 
 class DoublePendulum
@@ -21,7 +28,8 @@ class DoublePendulum
     const float damp = 1.0;
     Pendulum p1;
     Pendulum p2;
-    DoublePendulum(Pendulum p1, Pendulum p2);
+    Color trailColor;
+    DoublePendulum(Pendulum pen1, Pendulum pen2, Color col) : p1{pen1}, p2{pen2}, trailColor(col) {}
     void moveObjects(float dt);
     void updatePos();
 };
